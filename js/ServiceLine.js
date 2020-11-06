66ce98d4a0f173f6fa85f1f65cf0b3b1de5efdcc
Айдар Асакеев <Aidar>
2020-11-06 13:43:22 +0600

    changed time

--- js/ServiceLine.js
+++ js/ServiceLine.js
@@ -1,49 +1,3 @@
-// $(document).ready(function() {
-// var sloganList = $('.slogan_list'),
-//     sloganItem = $('.slogan_item'),
-//     active = $('sloganActive'),
-//     request = $('.request'),
-//     slider = $('.slider'),
-//     t = 0
-
-                                // Slider #1
-
-// var timer = setInterval(feedback, 5000)
-// function feedback(){
-//     sloganList.css({'margin-left' : '-=100%',
-//                     'transition' : '.5s'})
-//   sloganItem[t].classList.remove('sloganActive')
-//     t++
-
-//   if (t == 1) {
-//     slider.css({"background" : "url('../images/slider2.jpeg') 0 0 no-repeat",
-//                 "background-size": "cover",
-//                 "transition" : "1.3s",
-//                 "background-position" : "bottom"})
-//   }
-//   else if (t == 2) {
-//     sloganList.css({'margin-left' : '-=200%',
-//                       'transition' : '.5s'})
-//     slider.css({"background" : "url('../images/slider3.jpeg') 0 0 no-repeat",
-//                 "background-size" : "cover",
-//                 "transition" : "1.3s",
-//                 })
-//   }
-//   else {
-//     slider.css({"background" : "url('../images/slider1.jpeg') 0 0 no-repeat",
-//                 "background-size" : "cover",
-//                 "transition" : "1.3s"})
-//   }
-
-//     if (t >= sloganItem.length) {
-//         sloganList.css({'margin-left' : '0',
-//                         'transition' : '1s'})
-//                t = 0
-//     }
-//   sloganItem[t].classList.add('sloganActive')
-// }
-// })
-
 let d = document
 let menuButton = d.getElementById('hamburger'),
     menu = d.getElementById('menu')
@@ -58,7 +12,7 @@ $('.slogan_list').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   autoplay: true,
-  autoplaySpeed: 2000,
+  autoplaySpeed: 500,
   fade: true,
   speed: 500,
 });
